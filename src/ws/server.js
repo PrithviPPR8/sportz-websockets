37f044fb6dad5a5defb2868e125913ec1a99706e
Prithviraj Patil <prithvirajpatil64@gmail.com>
2026-03-02 10:36:04 +0530

    Arcjet 2:38:48

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../arcjet.js";
 
 function sendJson(socket, payload) {
     if(socket.readyState !== WebSocket.OPEN) return;
@@ -21,7 +22,25 @@ export function attachWebSocketServer(server) {
         maxPayload: 1024 * 1024, //1 MB
     });
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', async (socket, req) => {
+        if(wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+
+                if(decision.isDenied()) {
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008;   //1013 - Try again later. Rate Limitted. 1008 - Policiy Violation. Bot Detected.
+                    const reason = decision.reason.isRateLimit() ? "Rate limit exceeded" : "Access denied";
+
+                    socket.close(code, reason);
+                    return;
+                }
+            } catch (e) {   
+                console.error("WS Connection Error", e);
+                socket.close(1011, 'Server Security Error');
+                return;
+            }
+        }
+
         socket.isAlive = true;
         socket.on('pong', () => { socket.isAlive = true; });
 
